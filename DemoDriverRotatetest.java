349974cb02bee36c380d2b33ae4ca5124a5f78c6
lokesh <lokeshbs1993@gmail.com>
2020-05-14 16:40:35 +0530

    added method tc_FindElementByAndroidAutomator

--- DemoDriverRotatetest.java
+++ DemoDriverRotatetest.java
@@ -1,10 +1,13 @@
 package com.tyss.appiumproject;
 
 import java.net.URL;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -127,7 +130,7 @@ public class DemoDriverRotatetest {
 		driver.tap(1, driver.findElementByAccessibilityId("Views"), 50);
 	}
 	
-	@Test(enabled = true)
+	@Test(enabled = false)
 	public void tc_TapOnCoOrdinates() throws Exception {
 		
 		DesiredCapabilities cap=new DesiredCapabilities();
@@ -145,5 +148,40 @@ public class DemoDriverRotatetest {
 		AndroidDriver driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), cap);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.tap(1, 159, 1788, 50);
+		Thread.sleep(3000);
+		//driver.swipe(568, 1778, 568, 393, 1000);
+		
+		Dimension dim = driver.manage().window().getSize();
+        int startx = dim.getWidth()/2;
+        int starty=(int)(dim.getHeight()*0.8);
+        int endx=startx;
+        int endy=(int)(dim.getHeight()*0.2);
+		driver.swipe(startx, starty, endx, endy, 1000);
+
+
+	}
+	
+	@Test(enabled = true)
+	public void tc_FindElementByAndroidAutomator() throws Exception {
+		
+		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Lenovo K8 Plus");
+		cap.setCapability(MobileCapabilityType.UDID, "HNB3B18T");
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.1.1");
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "appium");
+		cap.setCapability(MobileCapabilityType.NO_RESET, true);
+		//cap.setCapability(MobileCapabilityType.FULL_RESET, true);
+		
+		cap.setCapability(MobileCapabilityType.APP,"G:\\Apkapp\\ApiDemos.apk");
+
+		
+		AndroidDriver driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), cap);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		  WebElement views = driver.findElementByAndroidUIAutomator("text(\"Views\")");
+		  views.click();
+		  Thread.sleep(3000);
+		  driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Seek Bar\"))").click();
+		  
 	}
 }
