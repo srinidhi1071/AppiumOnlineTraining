4845452a3aa179486e640ed57a0b19e6a2f798bb
shrutinilosey2812 <niloseyshruti2801@gmail.com>
2020-05-18 12:34:25 -0700

    Assignments

--- BigBasket.java
+++ BigBasket.java
@@ -8,9 +8,9 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidKeyCode;
 
-public class ApiDemos 
-{
+public class BigBasket {
 	@Test
 	public void enterValue() throws MalformedURLException, InterruptedException
 	{
@@ -19,14 +19,13 @@ public class ApiDemos
 		cap.setCapability("platformName", "android");
 		cap.setCapability("platformVersion", 9);
 		cap.setCapability("automationName", "appium");
-		cap.setCapability("noReset", true);
-		cap.setCapability("appPackage", "io.appium.android.apis");
-		cap.setCapability("appActivity", "ApiDemos");
+		cap.setCapability("noReset", true);//-->do not delete the cache memory after execution
+		cap.setCapability("appPackage", "com.bigbasket.mobileapp");
+		cap.setCapability("appActivity", "activity.SplashActivity");
 		AndroidDriver driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), cap);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.findElementByAccessibilityId("Views").click();
-		driver.findElementByAccessibilityId("Controls").click();
-		driver.findElementByAccessibilityId("3. Holo Light Theme").click();
-		driver.findElementById("io.appium.android.apis:id/edit").sendKeys("EnterTheValue");
+		driver.findElementById("com.bigbasket.mobileapp:id/homePageSearchBox").click();
+		driver.findElementById("com.bigbasket.mobileapp:id/searchView").sendKeys("Mangoes");
+		driver.pressKeyCode(AndroidKeyCode.ENTER);
 	}
 }
