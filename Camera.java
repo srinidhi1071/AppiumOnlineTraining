1e99ea0d21e590ea76b35c2ac41cc0806302eadf
Pooja Nehare <pooja.nehare.123@gmail.com>
2020-05-24 18:12:08 +0530

    Camera Script

---
+++ Camera.java
@@ -0,0 +1,34 @@
+package com.tyss.Appium.tests;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidKeyCode;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class Camera {
+	
+	public void launch_Camera() throws MalformedURLException
+	{
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Honor 10 Lite");
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10");
+		cap.setCapability(AndroidMobileCapabilityType.APP_PACKAGE,"com.android.deskclock");
+		cap.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "AlarmsMainActivity");
+		
+		URL url = new URL("http://localhost:4723/wd/hub");
+		AndroidDriver driver = new AndroidDriver(url, cap);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.pressKeyCode(AndroidKeyCode.KEYCODE_CAMERA);
+		
+	}
+	
+
+}
